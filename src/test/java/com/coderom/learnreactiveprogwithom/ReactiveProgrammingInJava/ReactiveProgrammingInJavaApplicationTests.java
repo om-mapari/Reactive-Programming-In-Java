0f0975ed62c8d8df26172756ee2a53a57bf25786
Om Mapari <mapariom05@gmail.com>
2023-05-14 03:19:28 +0530

    main branch

---
+++ src/test/java/com/coderom/learnreactiveprogwithom/ReactiveProgrammingInJava/ReactiveProgrammingInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderom.learnreactiveprogwithom.ReactiveProgrammingInJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProgrammingInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
